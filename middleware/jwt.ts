4f0bedeeb8a3ca32a13d4de473bae9ebc60c29a1
ชื่อคุณ <>
2025-09-11 17:23:30 +0700

    อัปเดตโค้ดล่าสุด

--- middleware/jwt.ts
+++ middleware/jwt.ts
@@ -1,11 +1,8 @@
 import { expressjwt } from "express-jwt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 export const secret = process.env.JWT_SECRET as string;
-
 export const jwtAuthen = expressjwt({
   secret: secret,
   algorithms: ["HS256"],
